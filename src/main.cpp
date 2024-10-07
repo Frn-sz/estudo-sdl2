c0849fee0257eed6539a119cf2523af5aa0ff9df
Frn-sz <fernandogdemedeirosw@gmail.com>
2024-10-06 22:41:35 -0300

    feat: Setting up OpenGL

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,20 @@
 using namespace std;
 
 void initialize_SDL();
+void set_default_window_attributes();
 
 int main(int argc, char const *argv[])
 {
     SDL_Window *window = nullptr;
 
     initialize_SDL();
+    set_default_window_attributes();
 
-    window = SDL_CreateWindow("SDL3 Test", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    window = SDL_CreateWindow("SDL3 Test", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress);
 
     if (!window)
         exit(1);
@@ -25,6 +31,8 @@ int main(int argc, char const *argv[])
 
     while (!quit)
     {
+        glViewport(0, 0, 600, 800);
+
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
@@ -50,6 +58,10 @@ int main(int argc, char const *argv[])
         {
             cout << "Right arrow is being pressed" << endl;
         }
+
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+        SDL_GL_SwapWindow(window);
     }
 
     SDL_DestroyWindow(window);
@@ -64,4 +76,13 @@ void initialize_SDL()
         cout << "SDL couldn't be initialized" << SDL_GetError();
     else
         cout << "SDL Initialized" << endl;
+}
+
+void set_default_window_attributes()
+{
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 }
